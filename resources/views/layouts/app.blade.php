a0238c252f7270219a5d694be3aa006267b35684
Rejaol karim <karimsalahuddin03@gmail.com>
2024-12-08 14:24:47 +0600

    refactor the index, app blade file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,17 +2,8 @@
 
 <x-base-layout :$title>
     
-    <x-layouts.header></x-layouts.header>
+    <x-layouts.header />
     {{$slot}}
-
-    <footer>
-       
-            <a href="#">Link1</a>
-            <a href="#">Link2</a>
-        {{$footerLinks}}
-    </footer>    
-
-
 </x-base-layout>
 
 
